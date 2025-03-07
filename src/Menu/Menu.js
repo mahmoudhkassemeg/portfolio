346648b8a9cd3acc18ecb7c45ec946a5fd91facf
test <mahmoudkassem@Mahmouds-MacBook-Air.local>
2025-03-07 06:33:38 +0200

    added channel name

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,26 +1,35 @@
-import React from 'react'
-import './Menu.css'
+import React from "react";
+import "./Menu.css";
 
-function Menu(){
-    
-    return(
-        <div className='main-menu'>
-            <div id='menu' className="Menu">
-            <div className='menuBar'></div>
-            <div className='menuBar'></div>
-            <div className='menuBar'></div>
-            </div>
+function Menu() {
+  function Mouseover() {}
+  function Mouseout() {}
+  return (
+    <div className="main-menu">
+      <div id="menu" className="Menu">
+        <div className="menuBar"></div>
+        <div className="menuBar"></div>
+        <div className="menuBar"></div>
+      </div>
 
-            <div  className='menu-content'>
-                <div id='menu-content' className='menu-container'></div>
-                    <div className='menu-items'>
-                    <div className='menu-home'><h4>Home</h4></div>
-                    <div className='menu-services'><h4>Services</h4></div>
-                    <div className='menu-contact'><h4>Contact</h4></div>
-                    <div className='menu-inquiry'><h4>Inquiry</h4></div>
-                </div>
-            </div>
-            
+      <div id="menu-content" className="menu-content">
+        <div id="menu-container" className="menu-container"></div>
+        <div id="menu-items" className="menu-items">
+          <div id="item" className="menu-home">
+            <h4><a href="#">Home</a></h4>
+          </div>
+          <div id="item" className="menu-services">
+            <h4><a href="#">Services</a></h4>
+          </div>
+          <div id="item" className="menu-contact">
+            <h4><a href="#">Contact</a></h4>
+          </div>
+          <div id="item" className="menu-inquiry">
+            <h4><a href="#">Inquiry</a></h4>
+          </div>
         </div>
-    )
-} export default Menu
+      </div>
+    </div>
+  );
+}
+export default Menu;
