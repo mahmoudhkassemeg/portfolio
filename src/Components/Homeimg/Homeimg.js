d3d68528c4c83ffdc55e77122e8a0fcb7f4b1f80
test <mahmoudkassem@Mahmouds-MacBook-Air.local>
2025-03-08 03:55:06 +0200

    added channel name

--- src/Components/Homeimg/Homeimg.js
+++ src/Components/Homeimg/Homeimg.js
@@ -1,34 +1,37 @@
-import './Homeimg.css'
-import logo from './img/img.jpg'
-import  'boxicons';
+import "./Homeimg.css";
+import logo from "./img/img.jpg";
+import "boxicons";
 
 function Homeimg() {
-  
   return (
-    
     <div className="homeImgMain">
-      
       <div className="home-content">
-        <h1 className='h1'>
+        <h1 className="h1">
           Hi it's<span> Mahmoud</span>
         </h1>
         <h3 className="text-animation">
-          i'm a <span className='span'></span>
+          i'm a <span className="span"></span>
         </h3>
-        <p className='p'>
-        Hello,
-        There is no limit in passion.
-        You can find my skills in almost any tech department.
-        Started from c++ passing throug Web Developer, IoT, 
-        Mechatronics to Video production and 3d Graphics.
-        Just send your inquiry to start as soon as possible
+        <p className="p">
+          Hello, There is no limit in passion. You can find my skills in almost
+          any tech department. Started from c++ passing throug Web Developer,
+          IoT, Mechatronics to Video production and 3d Graphics. Just send your
+          inquiry to start as soon as possible
         </p>
 
         <div className="social-icons">
-          <a href="#"><i className='bx bxl-linkedin' ></i></a>
-          <a href="#"><i className='bx bxl-github' ></i></a>
-          <a href="#"><i className='bx bxl-instagram' ></i></a>
-          <a href="#"><i className='bx bxl-twitter' ></i></a>
+          <a href="#">
+            <i className="bx bxl-linkedin"></i>
+          </a>
+          <a href="#">
+            <i className="bx bxl-github"></i>
+          </a>
+          <a href="#">
+            <i className="bx bxl-instagram"></i>
+          </a>
+          <a href="#">
+            <i className="bx bxl-twitter"></i>
+          </a>
         </div>
 
         <div className="btn-group">
@@ -39,10 +42,9 @@ function Homeimg() {
             Contact
           </a>
         </div>
-      </div>
-
-      <div className="home-img">
-        <img src={logo} alt="" />
+        <div className="home-img">
+          <img src={logo} alt="" />
+        </div>
       </div>
     </div>
   );
